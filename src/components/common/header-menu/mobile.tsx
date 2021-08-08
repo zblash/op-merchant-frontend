26ae9106ce52d00d104e833022b5f4fcfd79e335
Yusuf Can Celik <yusufcancelik@hotmail.com>
2021-08-08 03:55:52 +0300

    table component's bug fixed, all pages tables changed into new table

--- src/components/common/header-menu/mobile.tsx
+++ src/components/common/header-menu/mobile.tsx
@@ -5,9 +5,15 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Logo from '@/assets/images/logo/flogo.png';
 import { FaUserCircle, FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 import { RiNotification2Fill, RiCloseFill } from 'react-icons/ri';
-import { AiOutlineHome, AiOutlineCreditCard, AiOutlineSetting } from 'react-icons/ai';
+import {
+  AiOutlineHome,
+  AiOutlineCreditCard,
+  AiOutlineSetting,
+  AiFillDownCircle,
+  AiOutlineLogout,
+} from 'react-icons/ai';
 import { BiNetworkChart, BiChevronDownCircle } from 'react-icons/bi';
-import { BsInboxes } from 'react-icons/bs';
+import { BsInboxes, BsDot } from 'react-icons/bs';
 import { GoPackage } from 'react-icons/go';
 import { useAuth } from '@/contexts/auth-context';
 import { UILink } from '@/components/ui';
@@ -54,8 +60,16 @@ function MobileHeaderMenu() {
                       <span />
                       <span />
                     </div>
-                    <FaUserCircle size={16} />
+                    <UILink to="/profile">
+                      <FaUserCircle size={16} />
+                    </UILink>
                     <RiNotification2Fill size={16} />
+                    <AiOutlineLogout
+                      size={16}
+                      onClick={() => {
+                        logout();
+                      }}
+                    />
                   </>
                 )}
               </Col>
@@ -103,22 +117,70 @@ function MobileHeaderMenu() {
                       <BiNetworkChart />
                       <UILink to="/merchant/customers">SISTEMDEKI MUSTERILER</UILink>
                     </li>
-                    <li className="d-flex justify-content-start align-items-center mt-4">
-                      <AiOutlineCreditCard />
-                      <UILink to="/merchant/customers">CARI ISLEMLER</UILink>
+                    <li className="d-flex justify-content-start flex-column align-items-center mt-4">
+                      <div className="w-100 d-flex justify-content-start">
+                        <AiOutlineCreditCard />
+                        <UILink to="">
+                          CARI ISLEMLER <AiFillDownCircle />
+                        </UILink>
+                      </div>
+                      <div className="menu_option w-100">
+                        <ul>
+                          <li className="d-flex justify-content-start align-items-center">
+                            <BsDot />
+                            <UILink to="/merchant/credits">Cariler</UILink>
+                          </li>
+                          <li className="d-flex justify-content-start align-items-center">
+                            <BsDot />
+                            <UILink to="/credit-activities">Cari Ekstreleri</UILink>
+                          </li>
+                          <li className="d-flex justify-content-start align-items-center">
+                            <BsDot />
+                            <UILink to="/obligation-activities">Sistem Cari Ekstreleri</UILink>
+                          </li>
+                        </ul>
+                      </div>
                     </li>
-                    <li className="d-flex justify-content-start align-items-center mt-4">
-                      <GoPackage />
-                      <UILink to="/merchant/customers">URUN ISLEMLERI</UILink>
+                    <li className="d-flex justify-content-start flex-column align-items-center mt-4">
+                      <div className="w-100 d-flex justify-content-start">
+                        <GoPackage />
+                        <UILink to="/merchant/customers">
+                          URUN ISLEMLERI <AiFillDownCircle />
+                        </UILink>
+                      </div>
+                      <div className="menu_option w-100">
+                        <ul>
+                          <li>
+                            <UILink to="/product-specifies">Tüm Ürünler</UILink>
+                          </li>
+                          <li>
+                            <UILink to="/add-product-specify">Yeni Urun Ekle</UILink>
+                          </li>
+                        </ul>
+                      </div>
                     </li>
 
                     <li className="d-flex justify-content-start align-items-center mt-4">
                       <BsInboxes />
                       <UILink to="/merchant/customers">SIPARISLERI GOR</UILink>
                     </li>
-                    <li className="d-flex justify-content-start align-items-center mt-4">
-                      <AiOutlineSetting />
-                      <UILink to="/merchant/customers">DESTEK ISLEMLERI</UILink>
+                    <li className="d-flex justify-content-start flex-column align-items-center mt-4">
+                      <div className="w-100 d-flex justify-content-start">
+                        <AiOutlineSetting />
+                        <UILink to="/merchant/customers">
+                          DESTEK ISLEMLERI <AiFillDownCircle />
+                        </UILink>
+                      </div>
+                      <div className="menu_option w-100">
+                        <ul>
+                          <li>
+                            <UILink to="/create-ticket">Destek Talebi Olustur</UILink>
+                          </li>
+                          <li>
+                            <UILink to="/my-tickets">Destek Taleplerim</UILink>
+                          </li>
+                        </ul>
+                      </div>
                     </li>
                   </ul>
                 </Col>
