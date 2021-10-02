bfc89c5bec61509530225e0e53cdb714ea1ef6b2
Yusuf Can Celik <yusufcancelik@hotmail.com>
2021-10-02 21:54:14 +0300

    order table changed

--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useParams, useHistory } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import styled, { colors, css } from '@/styled';
-import { UIContainer } from '@/components/ui';
+import { UIContainer, UITableComponent } from '@/components/ui';
 import { useGetOrder } from '@/queries/use-get-order';
 import { useOrderConfirmMutation } from '@/queries/mutations/use-order-confirm';
 import { useUpdateOrderMutation } from '@/queries/mutations/use-update-order';
@@ -21,20 +21,6 @@ interface OrderItem extends IOrderItems {
 /* OrderPage Constants */
 
 /* OrderPage Styles */
-const StyledOrderItemTable = styled.table`
-  border-collapse: collapse;
-  border-spacing: 0;
-  margin-top: 15px;
-  width: 100%;
-  > th,
-  td {
-    text-align: center;
-    padding: 8px;
-  }
-  > tbody > tr:nth-child(odd) {
-    background-color: ${colors.lightGray};
-  }
-`;
 const StyledOrderActionsWrapper = styled.div`
   margin: 15px 1em 15px 0;
   width: 20%;
@@ -125,109 +111,130 @@ function OrderPage(props: React.PropsWithChildren<OrderPageProps>) {
     <UIContainer className="order_details">
       {order && !orderLoading && !error && (
         <>
-          <Row className="buyer_details">
-            <Col className="order_detail_text_wrapper my-4" xl={12} lg={12} sm={12} md={12}>
-              <h5>Siparis Detayi</h5>
-            </Col>
-            <Col xl={6} lg={6} sm={12} md={6}>
-              <div>
-                <h5>Alici</h5>
-                <p>{order.buyerName}</p>
-              </div>
-            </Col>
-            <Col className="d-flex justify-content-end" xl={6} lg={6} sm={12} md={6}>
-              <div>
-                <h5>Sehir / Ilce</h5>
-                <p>
-                  {order.buyerAddress.cityName} / {order.buyerAddress.stateName}
-                </p>
-              </div>
-            </Col>
-            <Col xl={6} lg={6} sm={12} md={6}>
-              <div>
-                <h5>Siparis Tarihi</h5>
-                <p>{order.orderDate}</p>
-              </div>
-            </Col>
-            <Col className="d-flex justify-content-end" xl={6} lg={6} sm={12} md={6}>
-              <div>
-                <h5>Siparis No</h5>
-                <p>{order.id.slice(0, 10)}</p>
-              </div>
-            </Col>
-            <Col xl={6} lg={6} sm={12} md={6}>
-              <div>
-                <h5>Siparis Durumu</h5>
-                <p>{t(`order.status.${order.status.toString().toLowerCase()}`)}</p>
-              </div>
-            </Col>
-            <Col className="d-flex justify-content-end" xl={6} lg={6} sm={12} md={6}>
-              <div>
-                <h5>Odeme Yontemi</h5>
-                <p>{t(`order.payment.${order.paymentType.toString().toLowerCase()}`)}</p>
-              </div>
-            </Col>
-            <Col xl={12} lg={12} sm={12} md={12}>
-              <div>
-                <h5>Adres Detayi</h5>
-                <p>{order.buyerAddress.details}</p>
-              </div>
-            </Col>
-            <Col xl={12} lg={12} sm={12} md={12}>
-              <div className="d-flex justify-content-between">
-                <h5>Toplam Fiyat</h5>
-                <h5>{order.totalPrice} TL</h5>
-              </div>
-            </Col>
+          <Row className="buyer_details_text_wrapper mb-5">
+            <Row className="w-100 m-0 ">
+              <Col className="order_detail_text_wrapper my-4" xl={12} lg={12} sm={12} md={12}>
+                <h5>Siparis Detayi</h5>
+              </Col>
+            </Row>
+            <Row className="w-100 m-0 d-flex justify-content-between py-2 buyer_details">
+              <Col className="d-flex justify-content-start" xl={2} lg={3} sm={3} md={3}>
+                <div>
+                  <h5>Alici</h5>
+                  <p>{order.buyerName}</p>
+                </div>
+              </Col>
+              <Col className="d-flex justify-content-start" xl={2} lg={3} sm={3} md={3}>
+                <div>
+                  <h5>Sehir / Ilce</h5>
+                  <p>
+                    {order.buyerAddress.cityName} / {order.buyerAddress.stateName}
+                  </p>
+                </div>
+              </Col>
+            </Row>
+            <Row className="w-100 m-0 d-flex justify-content-between py-2 buyer_details">
+              <Col className="d-flex justify-content-start" xl={2} lg={3} sm={3} md={3}>
+                <div>
+                  <h5>Siparis Tarihi</h5>
+                  <p>{order.orderDate}</p>
+                </div>
+              </Col>
+              <Col className="d-flex justify-content-start" xl={2} lg={3} sm={3} md={3}>
+                <div>
+                  <h5>Siparis No</h5>
+                  <p>{order.id.slice(0, 10)}</p>
+                </div>
+              </Col>
+            </Row>
+            <Row className="w-100 m-0 d-flex justify-content-between py-2 buyer_details">
+              <Col className="d-flex justify-content-start" xl={2} lg={3} sm={3} md={3}>
+                <div>
+                  <h5>Siparis Durumu</h5>
+                  <p>{t(`order.status.${order.status.toString().toLowerCase()}`)}</p>
+                </div>
+              </Col>
+              <Col className="d-flex justify-content-start" xl={2} lg={3} sm={3} md={3}>
+                <div>
+                  <h5>Odeme Yontemi</h5>
+                  <p>{t(`order.payment.${order.paymentType.toString().toLowerCase()}`)}</p>
+                </div>
+              </Col>
+            </Row>
+            <Row className="w-100 m-0 d-flex justify-content-between py-2 buyer_details">
+              <Col xl={12} lg={12} sm={12} md={12}>
+                <div>
+                  <h5>Adres Detayi</h5>
+                  <p>{order.buyerAddress.details}</p>
+                </div>
+              </Col>
+            </Row>
+            <Row className="w-100 m-0 d-flex justify-content-between py-2 my-4 buyer_details total_price_wrapper">
+              <Col xl={12} lg={12} sm={12} md={12}>
+                <div className="d-flex justify-content-between">
+                  <h5>Toplam Fiyat</h5>
+                  <h5>{order.totalPrice} TL</h5>
+                </div>
+              </Col>
+            </Row>
           </Row>
 
           <div className={overFlowAuto}>
-            <StyledOrderItemTable>
-              <thead>
-                <tr>
-                  <th>Urun Ismi</th>
-                  <th>Birim Fiyat</th>
-                  <th>Toplam Fiyat</th>
-                  <th>Indirim</th>
-                  <th>Indirimli Toplam Fiyat</th>
-                  <th>T.E.S Fiyat</th>
-                  <th>Toplam Siparis</th>
-                  <th>Kaldir</th>
-                </tr>
-              </thead>
-              <tbody>
-                {orderItems &&
-                  orderItems.map(orderItem => (
-                    <tr key={orderItem.id}>
-                      <td>1</td>
-                      <td>{orderItem.productName}</td>
-                      <td>{orderItem.unitPrice}</td>
-                      <td>{orderItem.totalPrice}</td>
-                      <td>{(orderItem.totalPrice - orderItem.discountedTotalPrice).toFixed(2)}</td>
-                      <td>{orderItem.discountedTotalPrice}</td>
-                      <td>{orderItem.recommendedRetailPrice}</td>
-                      <td>
-                        <input
-                          className={quantityInput}
-                          type="number"
-                          value={orderItem.quantity}
-                          name={orderItem.id}
-                          onChange={handleQuantityChange}
-                        />
-                      </td>
-
-                      <td>
-                        <input
-                          type="checkbox"
-                          name={orderItem.id}
-                          checked={orderItem.isRemoved}
-                          onChange={handleItemRemove}
-                        />
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </StyledOrderItemTable>
+            <UITableComponent
+              columns={[
+                {
+                  Header: 'Urun No',
+                  accessor: 'id',
+                  customRenderer: (item: OrderItem) => item.id.slice(0, 10),
+                },
+                {
+                  Header: 'Urun Ismi',
+                  accessor: 'productName',
+                },
+                {
+                  Header: 'Birim Fiyat',
+                  accessor: 'unitPrice',
+                },
+                {
+                  Header: 'Toplam Fiyat',
+                  accessor: 'totalPrice',
+                },
+                {
+                  Header: 'Indirim',
+                  accessor: 'discount',
+                  customRenderer: (item: OrderItem) => (item.totalPrice - item.discountedTotalPrice).toFixed(2),
+                },
+                {
+                  Header: 'Indirimli Toplam Fiyat',
+                  accessor: 'discountedTotalPrice',
+                },
+                {
+                  Header: 'T.E.S Fiyat',
+                  accessor: 'recommendedRetailPrice',
+                },
+                {
+                  Header: 'Toplam Siparis',
+                  accessor: 'quantity',
+                  customRenderer: (item: OrderItem) => (
+                    <input
+                      className={quantityInput}
+                      type="number"
+                      value={item.quantity}
+                      name={item.id}
+                      onChange={handleQuantityChange}
+                    />
+                  ),
+                },
+                {
+                  Header: 'Kaldir',
+                  accessor: 'remove',
+                  customRenderer: (item: OrderItem) => (
+                    <input type="checkbox" name={item.id} checked={item.isRemoved} onChange={handleItemRemove} />
+                  ),
+                },
+              ]}
+              data={orderItems}
+            />
             {(order.status === 'CANCEL_REQUEST' || order.status === 'NEW') && (
               <StyledOrderActionsWrapper>
                 <Button className={cancelButton} onClick={handleCancelRequest}>
